673afe71b851e6ad81de75795b03d12a3b2c9b32
Pablo <96089514+pabloz44@users.noreply.github.com>
2025-05-16 19:34:25 +0000

    prueba 2

--- src/trabajopractico/TrabajoPractico.java
+++ src/trabajopractico/TrabajoPractico.java
@@ -2,7 +2,7 @@ package trabajopractico;
 
 public class TrabajoPractico {
     public static void main(String[] args) {
-        System.out.println("Prueba");
+        System.out.println("Prueba 2");
     }
     
 }
